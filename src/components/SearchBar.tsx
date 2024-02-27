bd641e3bfa8d97d7d8a47dc0989c1435bfaa7273
anandafn <anandanamira03@gmail.com>
2024-02-27 09:19:19 +0700

    resolved bugs

--- src/components/SearchBar.tsx
+++ src/components/SearchBar.tsx
@@ -19,7 +19,7 @@ type Props = {
   onSubmit: (formData: SearchForm) => void;
   placeHolder: string;
   onReset?: () => void;
-  searchQuery: string;
+  searchQuery?: string;
 };
 
 const SearchBar = ({ searchQuery, onSubmit, placeHolder, onReset }: Props) => {
